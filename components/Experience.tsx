0243eea634c0f745246aa84987476ffed928bf72
dharanish-at <dharanishpmmcas@gmail.com>
2025-06-06 00:09:03 +0530

    c

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -17,7 +17,7 @@ const Experience: React.FC<{}> = () => {
               src={"/WebHR.svg"}
               height={30}
               width={30}
-              alt={"Dhara - UX Experience"}
+              alt={"Dharanishvnd - UX Experience"}
             />
             <p className="text-gray-300 ">
               <span className="font-semibold">WebHR /</span> Software Engineer
